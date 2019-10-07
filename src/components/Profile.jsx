4bdb2b022671d4ffb04af12dd1b8c783374e1d4a
hrudkouski <aprilshower19@gmail.com>
2019-10-07 17:20:15 +0300

    first commit - layout, grid, components (header, navBar profile)

---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className="content">
+        <div>
+            <img
+                alt='img content'
+                src='https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fs3-ap-northeast-1.amazonaws.com%2Fpsh-ex-ftnikkei-3937bb4%2Fimages%2F1%2F1%2F4%2F9%2F21329411-1-eng-GB%2F0699279.jpg?source=nar-cms'/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            my posts
+            <div>
+                new posts
+            </div>
+            <div>Post1</div>
+            <div>Post2</div>
+            <div>Post3</div>
+        </div>
+    </div>
+};
+
+export default Profile;
