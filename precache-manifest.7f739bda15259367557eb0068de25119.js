9093938eee4fa18696c8ef189e446ab3e14d330c
hrudkouski <aprilshower19@gmail.com>
2019-10-07 12:14:27 +0300

    Updates

---
+++ precache-manifest.7f739bda15259367557eb0068de25119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd071bbb3c3e757798d4c13a2372ae0c",
+    "url": "/social-network-react/index.html"
+  },
+  {
+    "revision": "b513925952276d2f7791",
+    "url": "/social-network-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fe227e7950750930e224",
+    "url": "/social-network-react/static/js/2.f6956817.chunk.js"
+  },
+  {
+    "revision": "b513925952276d2f7791",
+    "url": "/social-network-react/static/js/main.5f6367eb.chunk.js"
+  },
+  {
+    "revision": "431c1769dcfac761f360",
+    "url": "/social-network-react/static/js/runtime-main.7f90e827.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/social-network-react/static/media/logo.25bf045c.svg"
+  }
+]);
