05ff10a9fe5225bbfe0834c15dbf60275d4f5916
hrudkouski <aprilshower19@gmail.com>
2019-10-11 22:52:01 +0300

    20.21 commit - NavLink, Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
-// import s from './Dialogs.module.css';
+import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Nikita
+                </div>
+                <div className={s.dialog}>
+                    Masha
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Petr
+                </div>
+                <div className={s.dialog}>
+                    Marat
+                </div>
+                <div className={s.dialog}>
+                    Pavel
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    My first message
+                </div>
+                <div className={s.message}>
+                    My second message
+                </div>
+                <div className={s.message}>
+                    My third message
+                </div>
+            </div>
         </div>
     );
 };
